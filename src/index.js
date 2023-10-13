f72e7644be8e56d9d1353dc79da446ec5d66b7e6
glebanuk <glebbong12387@gmail.com>
2023-10-13 16:33:48 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,37 @@
 module.exports = function toReadable (number) {
-  
+    if (number === 0) {
+        return 'zero';
+    }
+
+    const ones = ['', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine'];
+    const teens = ['ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen'];
+    const tens = ['', '', 'twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety'];
+
+    const hundredsDigit = Math.floor(number / 100);
+    const tensDigit = Math.floor((number % 100) / 10);
+    const onesDigit = number % 10;
+
+    let result = '';
+
+
+    if (hundredsDigit > 0) {
+        result += ones[hundredsDigit] + ' hundred';
+    }
+
+    if (tensDigit === 1) {
+        result += (result.length > 0 ? ' ' : '') + teens[onesDigit];
+    } else {
+        if (tensDigit > 0) {
+            result += (result.length > 0 ? ' ' : '') + tens[tensDigit];
+        }
+        if (onesDigit > 0) {
+            result += (result.length > 0 ? ' ' : '') + ones[onesDigit];
+        }
+    }
+
+    return result;
 }
+    
+    
+    
+
